ddea46d35a1dd6c46eb15136d10e05af1cdf4c11
Vinicius Costa <66764951+Vini-VCosta@users.noreply.github.com>
2021-08-16 23:29:25 -0300

    Projeto RPG (criação de personagem)
    Versão 1.0 do projeto Final de Mobile 2

---
+++ projeto_final/lib/formulario.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Formulario());
+
+class Formulario extends StatelessWidget {
+  const Formulario({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Nome do Jogador'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Esta em branco';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Nome do Personagem'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Esta em branco';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Descrição do Personagem'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Esta em branco';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Cadastrando')),
+                  );
+                }
+              },
+              child: const Text('Cadastrar'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ projeto_final/lib/home/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_final/formulario.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Inicio(),
+  ));
+}
+
+//Classe/Tela Inicio, tela que inicia o app
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+            children: <Widget>[
+          Card(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("imagens/capa.jpg"),
+                ],
+              ),),
+            ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+          ),
+          Text('Bem Vindo ao projeto em desenvolvimento do nosso app, aqui você aprenderá um pouco sobre o universo onde o '
+                  'RPG se passa e podeá criar um personagem para participar de incriveis aventuras conosco, está pronto?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.red)),
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+          ),
+
+          // ElevatedButton(
+          //   child: Text('Vamos Começar', style: TextStyle(color: Colors.white)),
+          //   onPressed: () {
+          //     Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => Menu()));
+          //   },
+          //   style: ButtonStyle(
+          //     backgroundColor :  MaterialStateProperty . all ( Colors.lightBlue),
+          //   ),),
+          ] )
+            );
+  }
+}
+
+
+
+
+
